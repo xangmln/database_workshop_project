88ef8f34040cf527869aebb5f1d597cb0b932955
smsamlee <smsamlee007@gmail.com>
2025-11-19 23:59:16 +0900

    fix: StaticPool 추가

--- test/conftest.py
+++ test/conftest.py
@@ -1,29 +1,24 @@
 import pytest
 from typing import Generator
-
+from sqlalchemy.orm import Session
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
 from app.main import app
 from app.core.db import Base
 from app.api.utils.deps import get_db
+from app.api import models
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def override_get_db() -> Generator:
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-app.dependency_overrides[get_db] = override_get_db
 
 @pytest.fixture(scope="function")
 def db_session() -> Generator:
@@ -35,7 +30,15 @@ def db_session() -> Generator:
         db.close()
         Base.metadata.drop_all(bind=engine)
 
-@pytest.fixture(scope="module")
-def client() -> Generator:
+@pytest.fixture(scope="function")
+def client(db_session: Session) -> Generator:
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            pass
+
+    app.dependency_overrides[get_db] = override_get_db
     with TestClient(app) as c:
         yield c
+    app.dependency_overrides.clear()
