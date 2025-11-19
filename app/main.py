63660d6886b95b4f37ae2ef6d54c06ad0b4628bb
smsamlee <smsamlee007@gmail.com>
2025-11-19 23:59:50 +0900

    feat: auth router 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 
+from app.api.routes import router
+
 app = FastAPI()
+
+app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def index():
